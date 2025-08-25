a0d21b4ddfe889436bf3a5a60d9592ef4c4fd540
Sietse <sietsevanderhoeven@gmail.com>
2025-08-25 14:09:40 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+//! # chess-interface
+//! I made this just for a small chess engine competition so that we have one unified trait to
+//! implement when making our engines.
+
+use chess::{Board, ChessMove};
+
+/// Trait that chess engines can implement.
+pub trait ChessEngine {
+    /// Name of the engine
+    const NAME: &'static str;
+    /// Authors of the engine
+    const AUTHORS: [&'static str];
+    /// Version of the engine
+    const VERSION: &'static str;
+    
+    /// This function will be used to let the engines make moves in the game.
+    fn next_move(&mut self, board: Board) -> ChessMove;
+}
