c79ebc60f19c306d03093629cb96007c73e2d0d0
Sietse <sietsevanderhoeven@gmail.com>
2025-08-26 12:35:53 +0200

    Bump to 0.5.0-beta.1 with ponder prototype

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,11 @@
 //! I made this just for a small chess engine competition so that we have one unified trait to
 //! implement when making our engines.
 
+#[cfg(all(feature = "implementer", feature = "user"))]
+compile_error!("You can't have both `implementer` and `user` features enabled.");
+
+use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
 use chess::{Board, ChessMove};
 use std::time::Duration;
 
@@ -16,6 +21,12 @@ pub trait ChessEngine: Default {
     /// Version of the engine
     const VERSION: &'static str;
 
+    /// Method for when the other player is making a move,
+    fn ponder(&mut self, board: &Board, cancel: PonderCancel) -> PonderResult {
+        _ = (board, cancel);
+        PonderResult::Cancelled
+    }
+
     /// This function will be used to let the engines make moves in the game.
     fn next_move(
         &mut self,
@@ -23,4 +34,47 @@ pub trait ChessEngine: Default {
         remaining_time: Duration,
         increment: Duration,
     ) -> ChessMove;
+}
+
+/// Struct that tells an engine if the `ponder` is canceled.
+#[derive(Debug, Clone)]
+pub struct PonderCancel {
+    cancelled: Arc<AtomicBool>,
+}
+
+impl PonderCancel {
+    /// Create a new `PonderCancel`
+    #[cfg(feature = "user")]
+    #[inline]
+    #[must_use]
+    pub fn new() -> Self {
+        Self {
+            cancelled: Arc::new(AtomicBool::new(false)),
+        }
+    }
+
+    /// Cancel the `ponder`
+    #[cfg(feature = "user")]
+    #[inline]
+    pub fn cancel(&self) {
+        self.cancelled.store(true, std::sync::atomic::Ordering::SeqCst);
+    }
+
+    /// Check if the `ponder` is cancelled
+    #[cfg(feature = "implementer")]
+    #[inline]
+    #[must_use]
+    pub fn is_cancelled(&self) -> bool {
+        self.cancelled.load(std::sync::atomic::Ordering::SeqCst)
+    }
+}
+
+/// Enum for the `ponder` to return.
+#[derive(Debug, Clone, Copy, Hash, Eq, PartialEq, Ord, PartialOrd)]
+pub enum PonderResult {
+    /// Ponder is cancelled. Returned if the engine user calls for a cancel, or if the engine wants
+    /// to abort the pondering
+    Cancelled,
+    /// Move to premove. Returned when you found a move you want to play during pondering.
+    Move(ChessMove),
 }
